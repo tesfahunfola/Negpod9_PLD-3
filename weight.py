56fd4762ae6a5ed15872055285fbe7fa336f75df
Nekesa <c.nekesa@alustudent.com>
2023-03-10 19:34:17 +0200

    weight of waste

--- weight.py
+++ weight.py
@@ -1,17 +1,32 @@
-#This function measures the weight of the waste according to type
+#This function measures the weight of the waste according to wastetype
 #The weight of the waste is in kgs
 
 def measureWeight(wasteType):
-
     if wasteType == 1:
         weight = int(input("Please enter the weight of the organic waste (in kg): "))
     elif wasteType == 2:
         weight = int(input("Please enter the weight of the plastic waste (in kg): "))
     elif wasteType == 3:
-        weight = int(input("Please enter the weight of the E-waste (in kg): "))
+        weight = int(input("Please enter the weight of the e-waste (in kg): "))
     elif wasteType == 4:
         weight = int(input("Please enter the weight of the glassware waste (in kg): "))
     else:
         weight = 0
-
     return weight
+
+print(measureWeight(1)) # Prompts user to enter weight of organic waste
+def measureWeight(wasteType):
+    if wasteType == 1:
+        weight = int(input("Please enter the weight of the organic waste (in kg): "))
+    elif wasteType == 2:
+        weight = int(input("Please enter the weight of the plastic waste (in kg): "))
+    elif wasteType == 3:
+        weight = int(input("Please enter the weight of the e-waste (in kg): "))
+    elif wasteType == 4:
+        weight = int(input("Please enter the weight of the glassware waste (in kg): "))
+    else:
+        weight = 0
+    return weight
+
+print(measureWeight(1)) # Prompts user to enter weight of organic waste
+
