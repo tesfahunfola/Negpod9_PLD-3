12ea10cfa6624c4d91cdf0604e84354951eab58e
Maxime <m.kwizera@alustudent.com>
2023-03-10 09:33:04 +0200

    Upload of Introduction.py

---
+++ README.md
@@ -0,0 +1,9 @@
+Introduction.py
+
+# The first print statement welcomes the user to the application
+
+# The second print statement Informs of the reference of numbers to the type of waste to dispose.
+
+# Option 1-4 introduces waste disposal options
+
+# the last option 'Exit' exits the application.
---
+++ introduction.py
@@ -0,0 +1,16 @@
+# The first print statement welcomes the user to the application
+# The second print statement Informs of the reference of numbers to the type of waste to dispose.
+# Option 1-4 introduces waste disposal options
+# the last option 'Exit' exits the application.
+
+def menu():
+    print('Welcome to Neg9 Solid Waste Management System.')
+    print('Choose a number corresponding to the type of waste to dispose.')
+    print('[1] Organic Waste')
+    print('[2] Plastic Waste')
+    print('[3] E-Waste')
+    print('[4] Glassware waste')
+    print('[0] Exit')
+
+
+menu()
